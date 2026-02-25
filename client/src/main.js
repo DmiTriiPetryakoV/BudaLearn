7429fe502fcef0fccb7ea85994d301a8ec3bf95f
DmiTriiPetryakoV <petrakovdima199@gmail.com>
2026-02-25 21:12:53 +0300

    router debag

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import router from './router'
+import router from './router/index.js'
 import './style.css'
 import App from './App.vue'
 
